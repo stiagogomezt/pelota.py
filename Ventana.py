95c5b8c34d1fcfb79867f3206822faf4222be7f9
stiagogomezt <159092779+stiagogomezt@users.noreply.github.com>
2024-03-12 15:34:09 -0500

    Add files via upload

---
+++ Ventana.py
@@ -0,0 +1,13 @@
+class Ventana:
+    
+    def __init__(self):
+        
+        self.color_fondo
+        self.alto
+        self.ancho
+        self.titulo
+        self.botones 
+        
+    def crear() :
+        pass
+    
