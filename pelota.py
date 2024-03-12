14ed36761c565f283b69dcf76abe53a9792d7d57
stiagogomezt <159092779+stiagogomezt@users.noreply.github.com>
2024-03-12 15:31:42 -0500

    Add files via upload

---
+++ pelota.py
@@ -0,0 +1,22 @@
+class pelota:
+    
+    def __init__(self):
+        
+        self.x
+        self.y
+        self.color
+        self.radio
+        self.tipo_de_relleno
+        
+    def mover() :
+        pass
+    
+    def rebotar():
+        pass 
+    
+    def dibujar ():
+        pass 
+    
+      
+
+
